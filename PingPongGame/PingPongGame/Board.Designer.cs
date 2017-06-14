c5789b5f7cc3e806812c21a0daee46973b3af5c9
okocsis90 <o.kocsis90@gmail.com>
2017-06-14 14:54:36 +0200

    a basic version of pingPong game added

---
+++ PingPongGame/PingPongGame/Board.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PingPongGame
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.levelProgress = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.GrayText;
+            this.player.Location = new System.Drawing.Point(75, 353);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(123, 15);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ball.Location = new System.Drawing.Point(287, 182);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(10, 10);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(510, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(513, 30);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(10, 13);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Level";
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.AutoSize = true;
+            this.levelLabel.Location = new System.Drawing.Point(16, 29);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(10, 13);
+            this.levelLabel.TabIndex = 5;
+            this.levelLabel.Text = "-";
+            // 
+            // levelProgress
+            // 
+            this.levelProgress.Location = new System.Drawing.Point(19, 46);
+            this.levelProgress.Name = "levelProgress";
+            this.levelProgress.Size = new System.Drawing.Size(100, 23);
+            this.levelProgress.TabIndex = 6;
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 420);
+            this.Controls.Add(this.levelProgress);
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Name = "Board";
+            this.Text = "PingPong 3000";
+            this.Load += new System.EventHandler(this.Board_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Board_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Board_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label levelLabel;
+        private System.Windows.Forms.ProgressBar levelProgress;
+    }
+}
+
---
+++ PingPongGame/PingPongGame/Board.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Board : Form
+    {
+        private int playerSpeed = 10;
+
+        private int ballVelX = 5;
+        private int ballVelY = 5;
+        private int playerVelX;
+
+        private int score;
+        private int level = 1;
+        private int levelChange = 2;
+
+        private bool pause;
+
+        public Board()
+        {
+            InitializeComponent();
+        }
+
+        private void Board_Load(object sender, EventArgs e)
+        {
+            scoreLabel.Text = score.ToString();
+            levelLabel.Text = level.ToString();
+        }
+
+        private void Board_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                playerVelX = -playerSpeed;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                playerVelX = playerSpeed;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                if (!pause)
+                {
+                    pause = true;
+                }
+                else
+                {
+                    pause = false;
+                }
+
+            }
+        }
+
+        private void Board_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                playerVelX = 0;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                playerVelX = 0;
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (!pause)
+            {
+                ball.Location = new Point(ball.Location.X + ballVelX, ball.Location.Y + ballVelY);
+                player.Location = new Point(player.Location.X + playerVelX, player.Location.Y);
+            }
+
+            if (ball.Location.Y + ball.Height >= Size.Height)
+            {
+                pause = true;
+                ball.Location = new Point(Width / 2, Height / 2);
+                MessageBox.Show("Your score was: " + score);
+                score = 0;
+                scoreLabel.Text = score.ToString();
+            }
+
+            if (ball.Location.X + ball.Width >= player.Location.X &&
+                ball.Location.X < player.Location.X + player.Width &&
+                ball.Location.Y + ball.Height >= player.Location.Y &&
+                ball.Location.Y + ball.Height < player.Location.Y + 2)
+            {
+                ballVelY *= -1;
+                score += 1;
+                scoreLabel.Text = score.ToString();
+                levelProgress.Value += 100/levelChange;
+
+                if (score % levelChange == 0)
+                {
+                    pause = true;
+                    ball.Location = new Point(Width / 2, Height / 2);
+                    MessageBox.Show("LEVEL UP");
+                    level += 1;
+                    levelLabel.Text = level.ToString();
+                    levelProgress.Value = 0;
+                }
+            }
+
+            if (ball.Location.Y <= 0)
+            {
+                ballVelY *= -1;
+            }
+
+            if (ball.Location.X <= 0 || ball.Location.X + ball.Width * 2 >= Width)
+            {
+                ballVelX *= -1;
+            }
+        }
+    }
+}
